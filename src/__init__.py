ff19315f951a9582a437c1196e2f1ed727e8b30e
Bilal Elmoussaoui <belmouss@redhat.com>
2024-05-31 15:58:58 +0200

    release: Use graphql instead of https

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Script to release new gtk-rs crates version.
-"""
-
-__version__ = "0.0.1"
-__author__ = "Guillaume Gomez"
-
-
-__all__ = ["consts", "github", "release", "utils", "args"]
--- src/release.py
+++ src/release.py
@@ -7,15 +7,91 @@ import shutil
 import sys
 import tempfile
 from os.path import join
+import os
+import requests
+import getopt
 
-# local imports
-import consts
-from args import Arguments
-from github import Github
-from utils import add_to_commit, clone_repo
-from utils import write_error
-from utils import commit, write_msg
-from utils import check_if_up_to_date
+REPOSITORIES = [
+    {"name": "gtk4-rs", "start-at": "2023-07-23", "end-at": "2024-02-04"},
+    {"name": "gtk-rs-core", "start-at": "2023-07-23", "end-at": "2024-02-04"},
+]
+
+
+def github_search(token, repo_name, start_date, end_date):
+    query = """
+query {
+  
+  search(query: "repo:gtk-rs/{repo_name} is:pr is:closed merged:{start_date}..{end_date} base:master sort:created-desc -author:app/dependabot", type: ISSUE, last: 100) {
+    edges {
+      node {
+        ... on PullRequest {
+          url 
+          title
+          mergedAt
+          author {
+            login
+          }
+        }
+      }
+    }
+  }
+}
+""".replace("{repo_name}", repo_name)
+    query = query.replace("{start_date}", start_date)
+    query = query.replace("{end_date}", end_date)
+    headers = {"Authorization": f"Bearer {token}"}
+    request = requests.post(
+        "https://api.github.com/graphql", json={"query": query}, headers=headers
+    )
+    if request.status_code == 200:
+        return request.json()["data"]["search"]["edges"]
+    else:
+        raise Exception(
+            "Query failed to run by returning code of {}. {}".format(
+                request.status_code, query
+            )
+        )
+
+
+def write_help():
+    print("release.py accepts the following options:")
+    print("")
+    print(" * -h | --help                  : display this message")
+    print(" * -t <token> | --token=<token> : give the github token")
+
+
+class Arguments:
+    def __init__(self):
+        self.token = None
+
+    @staticmethod
+    def parse_arguments(argv):
+        try:
+            opts = getopt.getopt(argv, "ht:m:c:", ["help", "token="])[
+                0
+            ]  # second argument is "args"
+        except getopt.GetoptError:
+            write_help()
+            return None
+
+        instance = Arguments()
+
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                write_help()
+                return None
+            if opt in ("-t", "--token"):
+                instance.token = arg
+            else:
+                print(f'"{opt}": unknown option')
+                print('Use "-h" or "--help" to see help')
+                return None
+        if instance.token is None:
+            # In this case, I guess it's not an issue to not have a github token...
+            print("Missing token argument.")
+            return None
+
+        return instance
 
 
 @contextmanager
@@ -32,27 +108,27 @@ def temporary_directory():
                 raise
 
 
-def write_merged_prs(merged_prs, contributors, repo_url):
+def write_merged_prs(merged_prs, contributors):
     content = ""
-    for merged_pr in reversed(merged_prs):
-        if merged_pr.title.startswith("[release] "):
-            continue
-        if merged_pr.author not in contributors:
-            contributors.append(merged_pr.author)
+    for merged_pr in merged_prs:
+        merged_pr = merged_pr["node"]
+        if merged_pr["author"]["login"] not in contributors:
+            contributors.append(merged_pr["author"]["login"])
         md_content = (
-            merged_pr.title.replace("<", "&lt;")
+            merged_pr["title"]
+            .replace("<", "&lt;")
             .replace(">", "&gt;")
             .replace("[", "\\[")
             .replace("]", "\\]")
             .replace("*", "\\*")
             .replace("_", "\\_")
         )
-        content += f" * [{md_content}]({repo_url}/pull/{merged_pr.number})\n"
+        content += f" * [{md_content}]({merged_pr['url']})\n"
     return content + "\n"
 
 
 def build_blog_post(temp_dir, token):
-    write_msg("=> Building blog post...")
+    print("=> Building blog post...")
 
     author = input("Enter author name: ")
     title = input("Enter title: ")
@@ -73,41 +149,32 @@ For the interested ones, here is the list of the merged pull requests:
 
 """
     contributors = []
-    git = Github(token)
-    oldest_date = None
-
-    for repo in consts.REPOSITORIES:
-        release_date = repo["date"]
+    for repo in REPOSITORIES:
         repo_name = repo["name"]
-        if oldest_date is None or release_date < oldest_date:
-            oldest_date = release_date
-        write_msg(f"Gettings merged PRs from {repo_name}...")
-        merged_prs = git.get_pulls(
-            repo_name, consts.ORGANIZATION, "closed", release_date, only_merged=True
-        )
-        write_msg(f"=> Got {len(merged_prs)} merged PRs")
+        merged_prs = github_search(token, repo_name, repo["start-at"], repo["end-at"])
+        print(f"=> Got {len(merged_prs)} merged PRs")
         if len(merged_prs) > 0:
-            repo_url = f"{consts.GITHUB_URL}/{consts.ORGANIZATION}/{repo_name}"
+            repo_url = f"https://github.com/gtk-rs/{repo_name}"
             content += f"[{repo_name}]({repo_url}):\n\n"
-            content += write_merged_prs(merged_prs, contributors, repo_url)
+            content += write_merged_prs(merged_prs, contributors)
 
-    write_msg("Gettings merged PRs from gir...")
-    merged_prs = git.get_pulls(
-        "gir", consts.ORGANIZATION, "closed", oldest_date, only_merged=True
+    print("Gettings merged PRs from gir...")
+    merged_prs = github_search(
+        token, "gir", REPOSITORIES[0]["start-at"], REPOSITORIES[0]["end-at"]
     )
-    write_msg(f"=> Got {len(merged_prs)} merged PRs")
+    print(f"=> Got {len(merged_prs)} merged PRs")
     if len(merged_prs) > 0:
-        repo_url = f"{consts.GITHUB_URL}/{consts.ORGANIZATION}/gir"
+        repo_url = f"https://github.com/gtk-rs/gir"
         content += f"All this was possible thanks to the [gtk-rs/gir]({repo_url}) project as well:"
         content += "\n\n"
-        content += write_merged_prs(merged_prs, contributors, repo_url)
+        content += write_merged_prs(merged_prs, contributors)
 
     content += "Thanks to all of our contributors for their (awesome!) work on this release:\n\n"
     # Sort contributors list alphabetically with case insensitive.
     contributors = sorted(contributors, key=lambda s: s.casefold())
     content += "\n".join(
         [
-            f" * [@{contributor}]({consts.GITHUB_URL}/{contributor})"
+            f" * [@{contributor}](https://github.com/{contributor})"
             for contributor in contributors
         ]
     )
@@ -115,30 +182,20 @@ For the interested ones, here is the list of the merged pull requests:
 
     current_date = time.strftime("%Y-%m-%d")
     file_name = join(
-        join(temp_dir, consts.BLOG_REPO), f"_posts/{current_date}-new-release.md"
+        join(temp_dir, "gtk-rs.github.io"), f"_posts/{current_date}-new-release.md"
     )
     try:
+        os.makedirs(os.path.dirname(file_name))
         with open(file_name, "w", encoding="utf-8") as outfile:
             outfile.write(content)
-            write_msg(f'New blog post written into "{file_name}".')
-        add_to_commit(consts.BLOG_REPO, temp_dir, [file_name])
-        commit(consts.BLOG_REPO, temp_dir, "Add new blog post")
+        print(f'New blog post written into "{file_name}".')
     except Exception as err:
-        write_error(f"build_blog_post failed: {err}")
-        write_msg(f"\n=> Here is the blog post content:\n{content}\n<=")
-    write_msg("Done!")
-
-
-def clone_website_repo(temp_dir):
-    write_msg("=> Cloning the repositories...")
-    if clone_repo(consts.BLOG_REPO, temp_dir, depth=1) is False:
-        write_error(f'Cannot clone the "{consts.BLOG_REPO}" repository...')
-        return []
-    write_msg("Done!")
+        print(f"build_blog_post failed: {err}")
+        print(f"\n=> Here is the blog post content:\n{content}\n<=")
+    print("Done!")
 
 
 def start(args, temp_dir):
-    clone_website_repo(temp_dir)
     build_blog_post(temp_dir, args.token)
     input(
         "Blog post generated, press ENTER to quit (it'll remove the tmp folder and "
@@ -150,11 +207,9 @@ def main(argv):
     args = Arguments.parse_arguments(argv)
     if args is None:
         sys.exit(1)
-    if check_if_up_to_date() is False:
-        return
-    write_msg("=> Creating temporary directory...")
+    print("=> Creating temporary directory...")
     with temporary_directory() as temp_dir:
-        write_msg(f'Temporary directory created in "{temp_dir}"')
+        print(f'Temporary directory created in "{temp_dir}"')
         start(args, temp_dir)
 
 
